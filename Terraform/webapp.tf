8bd4d1a96af980b7cc752c71089268739190467d
Sriramdas Balaji <sriramdas.balaji@outlook.com>
2019-08-13 07:15:17 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,3 +1,12 @@
+ terraform {
+  required_version = ">= 0.11" 
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+	access_key  ="__storagekey__"
+	}
+	}
 resource "azurerm_resource_group" "dev" {
   name     = "PULTerraform"
   location = "West Europe"
