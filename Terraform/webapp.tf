0892dd233bc762e0c482a62d0f0afbaaeee6376f
Femi Ogunsakin <peace4eva74@gmail.com>
2022-09-28 22:57:13 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -2,7 +2,7 @@
   required_version = ">= 0.11" 
  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
-    container_name       = "terraform"
+    container_name       = "terraformfemi43"
     key                  = "terraform.tfstate"
 	access_key  ="__storagekey__"
 	}
